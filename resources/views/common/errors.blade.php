b9087e88adfd98e8fcc9c9506349cd04bb0008ee
3A532083 <luckyday604@gm.student.ncutedu.tw>
2018-10-21 13:50:39 +0800

    建構佈局與tasks視圖

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // 建立該任務...
+});
