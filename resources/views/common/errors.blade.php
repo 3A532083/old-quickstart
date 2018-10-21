52a6dd8e21bbaaf697593a5f5a6b7f55ef133233
3A532083 <luckyday604@gm.student.ncutedu.tw>
2018-10-21 13:59:27 +0800

    錯誤訊息

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -18,3 +18,16 @@ Route::post('/task', function (Request $request) {
     return redirect('/');
 
 });
+
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+        <br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
